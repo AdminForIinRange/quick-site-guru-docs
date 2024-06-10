1c9552cd35b224a4884d577ddfc2773726d279ba
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-06-11 01:18:46 +0930

    good night

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import BreadCrumbs from '../breadCrumbs/breadCrumbs'
 
 const Main = ({titlepath, subpath}) => {
-
+//fix no emjoi issues by if else or some other methosds like laocl store on click save variable
   return (
     <>
     
     <Box w={"1200px"} h={"700px"}  bg={"#242526"} rounded={"15px"} >
   <BreadCrumbs titlePath={titlepath} subPath={subpath} />
 
-  
+
 </Box></>
   )
 }
