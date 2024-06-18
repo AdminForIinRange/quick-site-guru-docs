b92fb432a32fc571c58666c88d6a8c3c149fb3b1
Anjesh <bhattaraianjesh123@gmail.com>
2024-06-18 19:49:04 +0930

    some imgs being added and link fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 "use client";
-import Main from "@/components/main/main";
-import Sidebar from "@/components/sidebar/sidebar";
+
 import { HStack, Box, useMediaQuery } from "@chakra-ui/react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
