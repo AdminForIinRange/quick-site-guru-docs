56cf41ed57105b63b69239003e8d9968cbfa0aa4
Anjesh <bhattaraianjesh123@gmail.com>
2024-06-18 19:56:12 +0930

    major Fixes

--- src/components/mainContent/getting-started/joiningDiscord.jsx
+++ src/components/mainContent/getting-started/joiningDiscord.jsx
@@ -1,12 +1,64 @@
 "use client";
 
-import { Box, VStack, Text, HStack } from "@chakra-ui/react";
+import {
+  Step,
+  StepDescription,
+  StepIcon,
+  StepIndicator,
+  StepNumber,
+  StepSeparator,
+  StepStatus,
+  StepTitle,
+  Stepper,
+  useSteps,
+  Box,
+  VStack,
+  Text,
+  HStack,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import BreadCrumbs from "@/components/breadCrumbs/breadCrumbs";
 import { HiArrowLongRight } from "react-icons/hi2";
 import { HiArrowLongLeft } from "react-icons/hi2";
+import Image from "next/image";
+
+import ReviewTitle from "../../../img/discord/ReviewTitle.png";
+import Review from "../../../img/discord/Review.png";
+import ReviewBody from "../../../img/discord/ReviewBody.png";
+import ReviewBody from "../../../img/discord/ReviewBody.png";
 
 const JoiningDiscord = ({ titlepath, subpath }) => {
+  const ReviewFormat = [
+    {
+      image: ReviewTitle,
+      desc: " When you're planning to upload your site for any of the stage reviews, make sure to follow this format Use your Discord name and your website name as the title. ",
+    },
+
+    {
+      image: ReviewBody,
+      desc: " Include your website link (which must be formatted in QSG and published with it ... This is how you can publish in QSG), and provide a description of your site, along with a image.",
+    },
+
+    {
+      image: Review,
+      desc: " This format ensures clarity and helps our team provide effective feedback to enhance your project, now all thats left is to wait for a mentor to respond.",
+    },
+    {
+      image: Review,
+      desc: " This format ensures clarity and helps our team provide effective feedback to enhance your project, now all thats left is to wait for a mentor to respond.",
+    },
+  ];
+
+  const { activeStep } = useSteps({
+    index: 4,
+    count: ReviewFormat.length,
+  });
   //fix no emjoi issues by if else or some other methosds like laocl store on click save variable
   return (
     <>
@@ -21,7 +73,7 @@ const JoiningDiscord = ({ titlepath, subpath }) => {
         pl={["18px", "25px", "25px", "15px", "35px"]}
         pr={["18px", "25px", "25px", "15px", "10px"]}
         py={"15px"}
-        pb={"50px"}
+        pb={"40px"}
       >
         <BreadCrumbs titlePath={titlepath} subPath={subpath} />
 
@@ -39,7 +91,7 @@ const JoiningDiscord = ({ titlepath, subpath }) => {
               fontWeight={"600"}
               fontSize={["38px", "38px", "38px", "38px", "48px"]}
             >
-              🧑‍💻 Introduction
+              🔊 Joining Discord
             </Text>
           </Box>
 
@@ -50,89 +102,71 @@ const JoiningDiscord = ({ titlepath, subpath }) => {
               lineHeight={"24px"}
               fontWeight={"300"}
             >
-              Hello and welcome to the official documentation for QuickSite.guru
-              – your gateway to effortless and innovative website creation! 🚀{" "}
-              <Text as={"span"} color={"orange.300"}>
-                Please Dont Skim it.{" "}
-              </Text>
-              This guide is your resource for understanding and utilizing all
-              the features of QuickSite.guru. Our platform makes web development
-              accessible to everyone, regardless of technical expertise. Let's
-              start building amazing websites! 🌐
-            </Text>
-          </Box>
-          <Box>
-            <Text
-              mb={"15px"}
-              color={"white"}
-              fontWeight={"600"}
-              fontSize={["18px", "18px", "20px", "20px", "28px"]}
-            >
-              About
-            </Text>
-            <Text
-              color={"white"}
-              fontSize={["14px", "14px", "14px", "14px", "16px"]}
-              lineHeight={"24px"}
-              fontWeight={"300"}
-            >
-              QuickSite.guru simplifies the complex process of website creation,
-              transforming it into a swift, seamless, and intuitive experience.
-              Our mission is to empower startups, small businesses, and
-              visionaries with the tools to establish a professional online
-              presence without hassle or high costs. 💼
+              Discord is the fundamental communication platform that we use for
+              sharing information, organising and moving forward. Make sure you
+              have it setup on both your mobile and desktop.
             </Text>
           </Box>
 
-          <Box>
+          <VStack h={"100%"} justify={"left"} align={"left"}>
             <Text
               mb={"15px"}
               color={"white"}
               fontWeight={"600"}
               fontSize={["18px", "18px", "20px", "20px", "28px"]}
             >
-              Our Product
+              What you need to do
             </Text>
-            <Text
-              color={"white"}
-              fontSize={["14px", "14px", "14px", "14px", "16px"]}
-              lineHeight={"24px"}
-              fontWeight={"300"}
+            <Stepper
+              h={"100%"}
+              w={"100%"}
+              index={activeStep}
+              orientation="vertical"
+              gap="0px"
+              colorScheme={"gray"}
             >
-              QuickSite.guru enables users to launch a website in just 5
-              minutes. We provide one year of free custom domain hosting (with
-              domain purchase) and use ChatGPT-like prompting for continuous
-              improvement. Our platform is designed for ease of use, offering
-              beautiful templates for quick cloning and hosting. Perfect for
-              startups and small businesses, our intuitive interface guides
-              users through the process effortlessly, ensuring a professional
-              online presence.
-            </Text>
-          </Box>
+              {ReviewFormat.map(({ image, desc }, index) => (
+                <Step key={index}>
+                  <StepIndicator>
+                    <StepStatus
+                      complete={<StepNumber />}
+                      incomplete={<StepNumber />}
+                      active={<StepNumber />}
+                    />
+                  </StepIndicator>
 
-          <Box>
-            <Text
-              mb={"15px"}
-              color={"white"}
-              fontWeight={"600"}
-              fontSize={["18px", "18px", "20px", "20px", "28px"]}
-            >
-              Getting Started
-            </Text>
-            <Text
-              color={"white"}
-              fontSize={["14px", "14px", "14px", "14px", "16px"]}
-              lineHeight={"24px"}
-              fontWeight={"300"}
-            >
-              To begin your journey with QuickSite.guru, follow our step-by-step
-              setup guide. This section will walk you through account creation,
-              selecting a template, customizing your site, and launching it
-              live. With clear instructions and helpful tips, you'll have your
-              website up and running in no time. Ready to get started? Let's
-              dive in!
-            </Text>
-          </Box>
+                  <Box
+                    w={["90%", "90%", "90%", "90%", "80%", "80%"]}
+                    flexShrink="0"
+                    mb={"20px"}
+                  >
+                    <StepTitle
+                      mb={"15px"}
+                      color={"white"}
+                      fontWeight={"300"}
+                      fontSize={["14px", "14px", "14px", "14px", "16px"]}
+                    >
+                      {desc}
+                    </StepTitle>
+
+                    <StepDescription mb={"15px"}>
+                      <Image
+                        style={{
+                          borderRadius: "15px",
+                          width: "100%",
+                          height: "100%",
+                        }}
+                        src={image}
+                        alt="image"
+                      />
+                    </StepDescription>
+                  </Box>
+
+                  <StepSeparator />
+                </Step>
+              ))}
+            </Stepper>
+          </VStack>
         </VStack>
       </Box>
     </>
