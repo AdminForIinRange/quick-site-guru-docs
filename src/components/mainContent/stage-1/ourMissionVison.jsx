04a3d3d6df61eddfe7e19fd01aadb9b4e5184e6b
Anjesh <bhattaraianjesh123@gmail.com>
2024-06-19 15:34:02 +0930

    adding UX

--- src/components/mainContent/stage-1/ourMissionVison.jsx
+++ src/components/mainContent/stage-1/ourMissionVison.jsx
@@ -5,6 +5,8 @@ import React, { useEffect, useState } from "react";
 import BreadCrumbs from "@/components/breadCrumbs/breadCrumbs";
 import { HiArrowLongRight } from "react-icons/hi2";
 import { HiArrowLongLeft } from "react-icons/hi2";
+import Link from "next/link";
+
 
 const OurMissionVison = ({ titlepath, subpath }) => {
   //fix no emjoi issues by if else or some other methosds like laocl store on click save variable
@@ -50,12 +52,30 @@ const OurMissionVison = ({ titlepath, subpath }) => {
               lineHeight={"24px"}
               fontWeight={"300"}
             >
-              Hello and welcome to the official documentation for QuickSite.guru
-              – your gateway to effortless and innovative website creation! 🚀
-              This guide is your resource for understanding and utilizing all
-              the features of QuickSite.guru. Our platform makes web development
-              accessible to everyone, regardless of technical expertise. Let's
-              start building amazing websites! 🌐
+              Welcome to       <Text
+          transition={"all 0.3s ease"}
+          mx={1.5}
+          as={Link}
+          target="_blank"
+          href="https://discord.com/channels/1223592781235359764/1224795787985420298"
+          cursor={"pointer"}
+          textAlign={"center"}
+          fontWeight={"bold"}
+          lineHeight={"35px"}
+          color={"white"}
+          p={1.5}
+          rounded={"8px"}
+          bg={"rgb(255, 255, 255,0.05)"}
+          border={"1px solid rgb(255, 255, 255,0.05)"}
+          _hover={{
+            transform: "scale(1.05)",
+            shadow: "0 0 25px 0px rgb(255, 255, 255,0.2)",
+          }}
+          whiteSpace={"nowrap"}
+        >
+          {" "}
+        quicksite.guru{" "}
+        </Text> – where your digital footprint is just a few clicks away from becoming a reality. QuickSite.guru emerges as a beacon of innovation in the website creation landscape, heralding a new era where accessibility, speed, and simplicity are not just ideals but everyday standards
             </Text>
           </Box>
           <Box>
@@ -65,7 +85,7 @@ const OurMissionVison = ({ titlepath, subpath }) => {
               fontWeight={"600"}
               fontSize={["18px", "18px", "20px", "20px", "28px"]}
             >
-              About
+              Our Mission
             </Text>
             <Text
               color={"white"}
@@ -73,11 +93,7 @@ const OurMissionVison = ({ titlepath, subpath }) => {
               lineHeight={"24px"}
               fontWeight={"300"}
             >
-              QuickSite.guru simplifies the complex process of website creation,
-              transforming it into a swift, seamless, and intuitive experience.
-              Our mission is to empower startups, small businesses, and
-              visionaries with the tools to establish a professional online
-              presence without hassle or high costs. 💼
+             In a world where an online presence is not just an asset but a necessity, QuickSite.guru aims to democratize website creation. We believe that every startup, every small business, and every visionary should have the tools to launch their dream online without the hassle, high costs, or time-consuming processes that have traditionally hampered digital aspirations.
             </Text>
           </Box>
 
@@ -88,7 +104,7 @@ const OurMissionVison = ({ titlepath, subpath }) => {
               fontWeight={"600"}
               fontSize={["18px", "18px", "20px", "20px", "28px"]}
             >
-              Getting Started
+           Our Vision
             </Text>
             <Text
               color={"white"}
@@ -96,12 +112,7 @@ const OurMissionVison = ({ titlepath, subpath }) => {
               lineHeight={"24px"}
               fontWeight={"300"}
             >
-              To begin your journey with QuickSite.guru, follow our step-by-step
-              setup guide. This section will walk you through account creation,
-              selecting a template, customizing your site, and launching it
-              live. With clear instructions and helpful tips, you'll have your
-              website up and running in no time. Ready to get started? Let's
-              dive in!
+             We envision a future where creating a professional, mobile-friendly website is as straightforward as asking for it. Leveraging the cutting-edge capabilities of AI, specifically in coding and design, QuickSite.guru transforms the complex web development process into an immediate, incremental, and interactive journey. Our goal is to disrupt the status quo, making static website hosting not just affordable but entirely free, and turning the dream of a custom online domain into a reality within minutes.
             </Text>
           </Box>
         </VStack>
